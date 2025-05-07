5cfb7028a73a192718ec6f1564b28510abbcbebc
vlad113chepurko <vlad113chepurko@gmail.com>
2025-05-07 15:02:54 +0300

    Added styles for header, icons, logo, navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,42 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
 export default function Header() {
   return (
     <header className="header">
-      <Link to="/">
-        <h1>Logo</h1>
-      </Link>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
+      <div className="logo-container">
+        <Link to="/">
+          <img className="logo" src="./ParkMarketLogo.svg" alt="logo" />
+        </Link>
+      </div>
       <div className="search-bar">
+        <button className="search-button">
+          <img
+            src="https://img.icons8.com/?size=100&id=132&format=png&color=FFFFFF"
+            alt="search"
+          />
+        </button>
         <input type="text" placeholder="Search..." />
-        <button>Search</button>
       </div>
-      <div className="cart-icon">
-        <Link to="/cart">Cart</Link>
-      </div>
-      <div className="user-info">
-        <p>User: John Doe</p>
-        <button>Logout</button>
-      </div>
+      <section className="icons-container">
+        <i className="cart-icon">
+          <Link to="/cart">
+            <img
+              className="icon"
+              src="https://img.icons8.com/?size=100&id=ii6Lr4KivOiE&format=png&color=FFFFFF"
+              alt="cart"
+            />
+          </Link>
+        </i>
+        <i className="profile-icon">
+          <Link to="/login">
+            <img
+              className="icon"
+              src="https://img.icons8.com/?size=100&id=98957&format=png&color=FFFFFF"
+              alt="user"
+            />
+          </Link>
+        </i>
+      </section>
     </header>
-  )
+  );
 }
