ee0653cb2a7570278a1a34534ae7fe85cfdb1081
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-05-08 12:55:49 +0300

    Added PUT method

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -17,3 +17,18 @@ router.post('/', verifyToken, async (req, res) => {
     res.status(500).json({ error: 'Failed to add product' });
   }
 });
+
+router.put('/:id', verifyToken, async (req, res) => {
+  try {
+    const { title, description, price, category, imageURL } = req.body;
+    
+    const updated_product = await Product.findByIdAndUpdate(req.params.id, { title, description, price, category, imageURL }, { new: true });
+
+    if (!updated_product) return res.status(404).json({ error: 'Product not found' });
+
+    res.status(200).json({ message: 'Product updated', product: updated_product });
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Failed to update product' });
+  }
+});
