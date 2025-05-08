92f19136587cc9a97570045f58aab782e8bdcb8a
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-05-08 21:06:57 +0300

    Create GET method for drinks

---
+++ backend/routes/product-filter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Products');
+const verifyToken = require('../middleware/authMiddleware');
+
+
+router.get('/drinks', verifyToken, async (req, res) => {
+  try {
+    const drinks = await Product.find({ category: 'Drinks' });
+    res.status(200).json(drinks);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Failed to fetch drinks' });
+  }
+});
