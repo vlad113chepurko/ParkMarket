aa0303d6406fef897b9be8b46dc763330cddfccd
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-05-08 15:20:01 +0300

    Fixed Product model

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
