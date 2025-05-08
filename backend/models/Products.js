ee8e221e8f79094ace9158b17c5a39383b403fbf
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-05-08 21:03:39 +0300

    Added temporary files

---
+++ backend/models/Products.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    imageURL: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Products', productSchema);
