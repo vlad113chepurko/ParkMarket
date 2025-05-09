55c05f7734345d74c2cf9a4b085eb7d955a2dd97
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-05-09 10:41:38 +0300

    Fixed connection

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const productFilterRoutes = require('./routes/product-filter');
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
