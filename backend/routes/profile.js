23fd49b14489c95ac3d50ba40bcb73510881d38c
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-05-13 20:31:16 +0300

    Added method for getting user data

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -21,6 +21,22 @@ const verifyToken = (req, res, next) => {
   }
 };
 
+router.get('/getUser', verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.userId)
+      .select('avatar name description');
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ message: "Failed to get user profile" });
+  }
+});
+
 router.put('/update', verifyToken, async (req, res) => {
   try {
     const { avatar, name, description, login, email, password } = req.body;
