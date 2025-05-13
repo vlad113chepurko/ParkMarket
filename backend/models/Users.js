bb3926c6653437c8063ffff788c076bed5ce4761
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-05-13 17:30:30 +0300

    Updated Users model

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  avatar: { type: String, default: "" },
+  description: { type: String, default: "" },
   login: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
