82d6aca53a2bf529f0cd22d7885d3afec5db0705
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-05-13 18:06:46 +0300

    Added field for name

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   avatar: { type: String, default: "" },
+  name: { type: String, default: "" },
   description: { type: String, default: "" },
   login: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
