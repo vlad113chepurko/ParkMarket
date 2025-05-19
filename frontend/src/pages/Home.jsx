1e9b569d13db095a5f5f6bc138c769fa06331dfe
vlad113chepurko <vlad113chepirko@gmail.com>
2025-05-19 13:49:13 +0300

    Rebase main page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,113 +7,40 @@ import "../styles/components/Zoom.css";
 // Components
 import ReactDOM from "react-dom";
 
-// Hooks/Context
-import { ModalContext } from "../context/ModalContext";
-import { useParkPhotoStore } from "../store/useParkPhotosStore";
-import { useState, useContext } from "react";
-
 export default function Home() {
-  // Photo params
-  const photos = useParkPhotoStore((state) => state.photos);
-  const [targetPhoto, setTargetPhoto] = useState("");
-  const [isZoom, setIsZoom] = useState(false);
-  const [titlePohoto, setPhotoTitle] = useState("");
-  const [descriptionPhoto, setDescriptionPhoto] = useState("");
-
-  const handleZoomPhoto = (photo) => {
-    setTargetPhoto(photo.src);
-    setPhotoTitle(photo.title);
-    setDescriptionPhoto(photo.description);
-    setIsZoom(true);
-  };
-
   return (
-    <ModalContext.Provider
-      value={{ isZoom, setIsZoom, targetPhoto, titlePohoto, descriptionPhoto }}
-    >
-      <div className="main-wrapper">
-        {isZoom ? <Zoom /> : null}
-        <div className="home">
-          <h1>Welcome to Our Park!</h1>
-          <div className="park-history">
-            <h2>About Park</h2>
-            <p>
-              Our park was founded in 1995 with the goal of creating a unique
-              space for relaxation and active recreation. Here, you can enjoy
-              nature, walk along the well-maintained paths, and participate in
-              various family-friendly events and activities.
-            </p>
-            <p>
-              Over the years, the park has grown and now offers visitors a wide
-              range of services: from walking zones to sports courts and
-              educational programs.
-            </p>
+    <div className="main-wrapper">
+      <div className="home">
+        <section className="video-container">
+          <div className="video-wrapper">
+            <video className="video" autoPlay loop muted>
+              <source src="/park-video.mp4" type="video/mp4" />
+              Your browser don't support video'
+            </video>
+            <article className="home-article">
+              <h1>Welcome to Our Park!</h1>
+              <h2>Order a tent, a bike, an electric scooter and relax!</h2>
+            </article>
           </div>
-
-          {/* Park History Section */}
-          <section className="park-history">
-            <h2>History of the Park</h2>
-            <p>
-              Our park opened in 1995 as a place for leisure. Over the years, it
-              has developed and modernized, and today it is one of the city's
-              most popular parks. In 2010, the park underwent a major
-              renovation, introducing new recreational areas and landscaping.
-              The park actively participates in environmental and cultural
-              projects.
-            </p>
-            <p>
-              In 2023, the park won an award as the best urban park for family
-              recreation.
-            </p>
+        </section>
+        <section className="order-container">
+          <section className="order-container-side">
+            <article className="align-left">
+              <h2>Make your first order</h2>
+              <h3>Go relax now!</h3>
+              <p>
+                We invite you to spend a wonderful time in our park with
+                available functions such as - ordering a bike, electric scooter,
+                gazebo by the lake.
+              </p>
+              <button className="order-button ">I wanna take order!</button>
+            </article>
           </section>
-
-          {/* Gallery Section */}
-          <section className="park-media">
-            <h2>Gallery</h2>
-            <section className="park-media-photos">
-              {photos.map((photo, index) => {
-                return (
-                  <i key={index}>
-                    <img
-                      onClick={() => handleZoomPhoto(photo)}
-                      className="park-photo"
-                      title={photo.description}
-                      src={photo.src}
-                      alt={photo.title}
-                      loading="lazy"
-                    />
-                  </i>
-                );
-              })}
-            </section>
+          <section className="order-container-side">
+            <img src="/trees-1587301_1920.jpg" alt="tree" />
           </section>
-        </div>
+        </section>
       </div>
-    </ModalContext.Provider>
-  );
-}
-
-function Zoom() {
-  const { isZoom, setIsZoom, targetPhoto, titlePohoto, descriptionPhoto } =
-    useContext(ModalContext);
-  if (!isZoom) return null;
-
-  return ReactDOM.createPortal(
-    <div className="zoom-overlay">
-      <div className="zoom">
-        <h2>{titlePohoto}</h2>
-        <p>{descriptionPhoto}</p>
-        <i className="zoom-image-container ">
-          <img className="zoom-image" src={targetPhoto} alt="image" />
-        </i>
-        <img
-          onClick={() => setIsZoom(false)}
-          className="close-button"
-          src="https://img.icons8.com/?size=100&id=71200&format=png&color=ec9a9a"
-          alt="close"
-        />
-      </div>
-    </div>,
-    document.body
+    </div>
   );
 }
